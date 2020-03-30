026f94f1583f15be5b3f4736168491f8e267b408
Florian Unzicker <unzico@gmail.com>
2020-03-30 16:00:48 +0200

    Test source control commit

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -1,16 +1,16 @@
-import {fromJS} from 'immutable';
-import {EDIT_USER, LOGOUT, SET_USER} from '../constants/actions';
+import { fromJS } from "immutable";
+import { EDIT_USER, LOGOUT, SET_USER } from "../constants/actions";
 
+// Prettier commit
 export const initialState = fromJS({
   id: null,
-  email: null
+  email: null,
 });
 
 export function user(state = initialState, action) {
   switch (action.type) {
     case SET_USER:
-      return state
-        .merge(action.payload);
+      return state.merge(action.payload);
 
     case EDIT_USER:
       return state.merge(action.payload);
