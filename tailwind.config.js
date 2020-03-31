94d4e1c623405c86f3a77a2ad464cef130e9128c
Florian Unzicker <unzico@gmail.com>
2020-03-31 22:18:04 +0200

    Add color palette from Figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,12 @@ module.exports = {
     },
     extend: {
       colors: {
+        "brand-light": "#94E1AE",
         brand: "#199057",
-        "primary-light": "#91B169",
-        "primary-dark": "#325802",
-        "secondary-light": "#CEEECE",
-        "secondary-dark": "#749A60",
-        "accent-light": "#F5E5D4",
-        "accent-dark": "#F2AC7D"
+        "brand-dark": "#197649",
+        "accent-light": "#DCB38E",
+        accent: "#7C4B36",
+        grey: "#EBEBEB"
       },
       height: {
         "100vh": "100vh"
