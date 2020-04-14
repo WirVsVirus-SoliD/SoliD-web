43f28c33fcac722a65b66d665ceaa4b130a29460
Florian Gerhardt <florian.gerhardt92@gmail.com>
2020-04-14 11:11:39 +0200

    Fix rebase conflict

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,8 @@ const domain = process.env.REACT_APP_API_PATH;
 export default {
   auth: {
     refresh: domain + "/auth/refresh",
-    login: domain + "/auth/login"
+    login: domain + "/auth/login",
+    validate: domain + "/auth/validate"
   },
   providers: {
     collection: domain + "/providers"
